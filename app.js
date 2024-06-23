75b4aceffe345fe6d405978178d04b7862541a20
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-23 14:05:13 +0700

    js lab 135

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-// write code here
+// จงสร้างฟังก์ชัน getAverageAge(arr) เพื่อหาอายุเฉลี่ยของ array
+
+let john = { name: 'John', age: 27 };
+let jo = { name: 'Jo', age: 21 };
+let jin = { name: 'Jin', age: 25 };
+
+let arr = [john,jo,jin];
+
+function getAverageAge(arr) {
+    let sum = 0
+    for (i = 0; i < arr.length; i++) {
+        sum +=(arr[i].age)
+    }
+    const averageAge =  (sum / arr.length).toFixed(2)
+    return averageAge
+}
+
+console.log(getAverageAge(arr)); // (27 + 21 + 25) / 3 = 24.33
